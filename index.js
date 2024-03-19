4f5652dfe6abfd63a33bb30d85ca09ede927eb3f
DawnBreaker <tunganhngo207@gmail.com>
2024-03-19 13:10:14 +0700

    Add: add env environment

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import router from './routes/index.js';
 import mongoose from 'mongoose';
-const PORT = 8888;
-const URL = 'mongodb://localhost:27017/Nodejs';
+import 'dotenv/config';
+const PORT = process.env.PORT;
+const URI = process.env.URI;
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', router);
 
 await mongoose
-  .connect(URL)
+  .connect(URI)
   .then(() => {
     console.log(`Welcome to MongoDB`);
+    app.listen(PORT, () => {
+      console.log(`Listen on port http://localhost:${PORT}/api/v1/products`);
+    });
   })
   .catch((err) => console.log(`Error connecting to MongoDB, error: ${err}`));
-
-app.listen(PORT, () => {
-  console.log(`Listen on port http://localhost:${PORT}/api/v1/products`);
-});
