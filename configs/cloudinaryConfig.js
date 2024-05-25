f10ca24d319c90edbc507f7aec7fb4b5f28ed6df
DawnBreaker <tunganhngo207@gmail.com>
2024-05-26 00:31:47 +0700

    fix bug issues

--- configs/cloudinaryConfig.js
+++ configs/cloudinaryConfig.js
@@ -1,10 +1,10 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { API_KEY, API_SECRET, CLOUD_NAME } from '../utils/env.js';
+// import { v2 as cloudinary } from 'cloudinary';
+// import { API_KEY, API_SECRET, CLOUD_NAME } from '../utils/env.js';
 
-cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: CLOUD_NAME,
+//   api_key: API_KEY,
+//   api_secret: API_SECRET,
+// });
 
-export default cloudinary;
+// export default cloudinary;
--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -1,19 +1,19 @@
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../configs/cloudinaryConfig.js';
+// import multer from 'multer';
+// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+// import cloudinary from '../configs/cloudinaryConfig.js';
 
-export const uploadImagesMiddleware = (fieldName, maxCount) => {
-  const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'nodejs',
-      format: 'jpg',
-    },
-  });
-  const upload = multer({ storage: storage });
-  return (req, res, next) => {
-    upload.array(fieldName, maxCount)(req, res, () => {
-      next();
-    });
-  };
-};
+// export const uploadImagesMiddleware = (fieldName, maxCount) => {
+//   const storage = new CloudinaryStorage({
+//     cloudinary: cloudinary,
+//     params: {
+//       folder: 'nodejs',
+//       format: 'jpg',
+//     },
+//   });
+//   const upload = multer({ storage: storage });
+//   return (req, res, next) => {
+//     upload.array(fieldName, maxCount)(req, res, () => {
+//       next();
+//     });
+//   };
+// };
